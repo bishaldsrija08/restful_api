8375f9cb8e34a03a928bf06d88a898bec47ddce5
bishaldsrija08 <bishaldsrijal1@gmail.com>
2026-01-08 21:05:25 +0545

    feat: add method-override package with debug and ms dependencies
    - Introduced method-override package version 3.0.0.
    - Added debug package version 3.1.0 with browser and Node.js implementations.
    - Included ms package version 2.0.0 for time conversion utilities.
    - Implemented Karma configuration for testing the debug package.
    - Added necessary files and documentation for all packages.

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 
+// method-override setup
+const methodOverride = require('method-override');
+
 const { v4: uuidv4 } = require('uuid');
 
 // Ejs setup
@@ -11,6 +14,7 @@ app.use(express.static('public'));
 
 // Middleware to parse URL-encoded bodies
 app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
 
 let posts = [{
     id: uuidv4(),
@@ -49,29 +53,48 @@ app.get("/", (req, res) => {
 // Get single post
 app.get("/posts/:id", (req, res) => {
     const id = req.params.id;
-    const post = posts.find(p => p.id ===id)
+
+    //     - Searches the `posts` array to find the post with the matching `id`.
+    // - `find()`:
+    //   - Loops through each post
+    //   - Returns the **first post** where `p.id === id`
+    // - If no post is found, `post` will be `undefined`.
+
+    const post = posts.find(p => p.id === id)
     res.render("post.ejs", { data: post, id: id })
 })
 
 // delete post
-app.get("/posts", (req, res) => {
-    const id = req.query.id;
-    posts.splice(id, 1);
+app.delete("/posts/:id", (req, res) => {
+    const id = req.params.id;
+// Removes the post with the matching ID.
+// filter():
+// Loops through each post p
+// Keeps only posts whose ID does NOT match the given id
+// The matching post is excluded, effectively deleting it.
+// A new array is created and reassigned to posts.    
+    posts = posts.filter((p)=> id!== p.id)
     res.redirect("/");
 })
 
 // get update form
 app.get("/posts/edit/:id", (req, res) => {
     const id = req.params.id;
-    const post = posts[id];
+    const post = posts.find(p => p.id === id)
     res.render("edit.ejs", { data: post, id: id })
 })
 
 // update post
-app.post("/posts/update/:id", (req, res) => {
+app.patch("/posts/update/:id", (req, res) => {
     const id = req.params.id
     const { username, title } = req.body;
-    posts[id] = { username, title };
+    console.log(username, title)
+    const post = posts.find(p => p.id === id)
+    console.log(post)
+    if (post) {
+        post.username = username;
+        post.title = title;
+    }
     res.redirect("/");
 })
 
